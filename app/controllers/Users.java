4b65715c82304674d4ece0e884ec4a8df79a6557
kumlien <svante.kumlien@gmail.com>
2013-09-10 16:43:39 +0200

    tuesdays work, trying to get a user register form up and running...

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,5 +1,8 @@
 package controllers;
 
+
+import javax.validation.Valid;
+
 import models.User;
 import play.data.Form;
 import play.mvc.Controller;
@@ -9,11 +12,28 @@ public class Users extends Controller {
 	
 	static final Form<User> productForm = Form.form(User.class);
 	
-	public static Result login() {
+	/**
+	 * Called from template via ajax, return as json
+	 */
+	public static Result login() throws InterruptedException {
 		Form<User> userForm = productForm.bindFromRequest();
-		User.authenticate(userForm.get().username, userForm.get().password);
-		session().clear();
-		session("username", userForm.get().username);
+		System.out.println(userForm.get());
+//		User.authenticate(userForm.get().username, userForm.get().password);
+//		session().clear();
+//		session("username", userForm.get().username);
+		Thread.sleep(3000);
 		return redirect(routes.Application.index());
+	}
+	
+	public static Result renderRegisterPage() {
+		return ok(views.html.register.render());
+	}
+	
+	public static Result register() {
+		Form<User> userForm = productForm.bindFromRequest();
+		User newUser = userForm.get();
+		System.out.println(newUser);
+		
+		return ok();
 	}
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,9 +1,16 @@
 package models;
 
 import java.util.Date;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import play.data.validation.Constraints.Min;
+import play.data.validation.Constraints.Required;
+import play.libs.F.Option;
+import play.mvc.QueryStringBindable;
 
 @Entity
 public class User {
@@ -11,13 +18,27 @@ public class User {
 	@Id
 	public String username;
 	
+	
 	public String firstName;
+	
 	
 	public String lastName;
 	
 	public Date joined;
 	
+	
 	public String password;
+	
+	@Transient
+	public String password2;
+	
+	public User(String username, String firstName, String lastName, String password, String password2) {
+		this.username=username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.password2 = password2;
+	}
 
 	@Override
 	public String toString() {
